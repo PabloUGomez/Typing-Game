01e348017d721599ed57f7ebaa238eae8ec773e0
Pablo Gomez <pablogomez0430@gmail.com>
2024-04-12 02:46:22 -0300

    add base

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'black': '#222',
+			'white': '#fff',
+			'gray': '#999',
+			'yellow': '#daaf38',
+			'green': '#00b755',
+			'red': '#ca4754',
+		},	
+		extend: {},
+	},
+	plugins: [],
+}
